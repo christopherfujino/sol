a1be5de043a92f1b5868e191ab452e148429ba64
Christopher Fujino <christopherfujino@gmail.com>
2022-10-10 16:44:47 -0700

    analysis cleanup

--- interpreter/test/error_files_test.dart
+++ interpreter/test/error_files_test.dart
@@ -3,7 +3,6 @@ import 'dart:io' as io;
 import 'package:sol/src/interpreter/interpreter.dart';
 import 'package:sol/src/parser/parser.dart';
 import 'package:sol/src/scanner.dart';
-import 'package:sol/src/source_code.dart';
 import 'package:test/test.dart';
 
 import 'common.dart';
